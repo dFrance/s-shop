7b3c3f247dd7206ca6bc515b09d07a5bc17aa79c
Gabriel de França <emaildogabrieldefranca@gmail.com>
2021-11-20 22:26:22 -0300

    update: drawer buttons

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -4,19 +4,55 @@ import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import { useStyles } from '../../components/Header/styles';
+import { Box, Button, Drawer, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core';
+import { useState } from 'react';
+import Icon from '@material-ui/icons';
+import SecurityIcon from '@material-ui/icons/Security';
+import HomeIcon from '@material-ui/icons/Home';
+import { useHistory } from 'react-router';
 
-export function Header(){
-    const classes = useStyles()
-    return(
-        <AppBar position="static" style={{boxShadow: 'none'}}>
+type DrawerItemsProps = {
+    text: string,
+    link: string,
+    index: string,
+}
+export function Header() {
+    const classes = useStyles();
+    const history = useHistory();
+    const [isOpen, setIsOpen] = useState(false);
+    function toggleDrawer() {
+        setIsOpen((old) => !old);
+    }
+
+    function handleUrlPage(link: any) {
+        history.push(link)
+    }
+    return (
+        <AppBar position="static" style={{ boxShadow: 'none' }}>
             <Toolbar variant="dense" className={classes.header}>
                 <Typography variant="h6" color="inherit">
                     S-Shop
                 </Typography>
-                <IconButton color="inherit" aria-label="Menu">
+                <IconButton onClick={() => toggleDrawer()} color="inherit" aria-label="Menu">
                     <MenuIcon />
                 </IconButton>
             </Toolbar>
+            <Drawer anchor="left" open={isOpen} onClose={() => toggleDrawer()}>
+                <Box role="presentation" width={320}>
+                    <ListItem button onClick={() => handleUrlPage('/')}>
+                        <ListItemIcon>
+                            <HomeIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Visitante" />
+                    </ListItem>
+                    <ListItem button onClick={() => handleUrlPage('/admin')}>
+                        <ListItemIcon>
+                            <SecurityIcon />
+                        </ListItemIcon>
+                        <ListItemText primary="Admin" />
+                    </ListItem>
+                </Box>
+            </Drawer>
         </AppBar>
     )
 }
